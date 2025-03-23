8b72340ff30938cd027410aeddb2b12b4826c13f
natsuki-saito <nasushian@gmail.com>
2025-03-24 03:35:32 +0900

    書籍一覧、貸出確認、貸出登録機能の実装

--- app/controllers/loans_controller.rb
+++ app/controllers/loans_controller.rb
@@ -1,16 +1,28 @@
 class LoansController < ApplicationController
   def index
-    @loans = Loan.all
+    #booksテーブルとloansテーブルを結合して、書籍タイトルと最新の貸し出し状況と返却日を変数@booksに代入する
+    #@books = Book.left_outer_joins(:loans).select('books.*, loans.returned_date, loans.schedule_date').distinct
+    #本のレコードとその本に関する貸出情報をすべて取得
+    @books = Book.includes(:loans).all
   end
 
   def new
-    @loan = Loan.new
+    #params[:id]を受け取り、find_byメソッドで該当する書籍を検索する
+    @book = Book.find_by(id: params[:id])
+    #新たな貸し出し情報に必要な項目（書籍、ログイン中の会員、返却予定日←これは1週間後のみ）貸出日はupdated_atレコードの更新日が基準
+    @loan = Loan.new(book: @book, user: current_user, schedule_date: 1.week.from_now.to_date)
   end
 
   def create 
-    @loan = Loan.new(loan_params)
-    @loan.save
-    redirect_to loans_path
+    #現在のユーザーに紐づいた貸出情報を作成する
+    #current_user.loansで現在ログイン中の会員と新しく作成される貸出情報が紐づけられる。（user_idが自動的に設定される）
+    @loan = current_user.loans.build(loan_params)
+    if @loan.save
+    redirect_to loans_path, notice: '貸出が完了しました。'
+    else
+      @book =  @loan.book
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,9 @@
 class Book < ApplicationRecord
   has_many :loans
+
+  def latest_loan
+    #その本に関連するすべてのloanレコードを取得
+    #取得したレコードを新しい順に並べ替え、そこから最新の1件を返す
+    loans.order(created_at: :desc).first
+  end
 end
