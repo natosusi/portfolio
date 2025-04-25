20232a4179b08c87441d132209f016c37f898e3c
natsuki-saito <nasushian@gmail.com>
2025-04-25 18:43:41 +0900

    add: booksテーブルにカラムを追加する #9

--- railsapp/db/schema.rb
+++ railsapp/db/schema.rb
@@ -10,11 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_04_14_173639) do
+ActiveRecord::Schema[7.2].define(version: 2025_04_25_091344) do
   create_table "books", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "title", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "author"
+    t.text "description"
+    t.string "image_link"
   end
 
   create_table "lendings", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
