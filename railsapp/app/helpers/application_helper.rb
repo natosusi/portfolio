8ba0e38d1490cb1c0591c577fe628872018b3562
natsuki-saito <nasushian@gmail.com>
2025-05-09 18:09:19 +0900

    会員詳細画面にお気に入り登録した書籍を表示させる #26

--- railsapp/app/helpers/application_helper.rb
+++ railsapp/app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
 module ApplicationHelper
+  #貸出中のユーザーとログインユーザーの一致判定
+  def latest_lending_user(book)
+    return true if book.latest_lending.user_id == current_user.id
+  end
+
+  #貸出状態の判定
+  def book_available?(book)
+    #その本の貸出情報が存在しない場合trueを返す
+    return true unless book.lendings.exists?
+    #最新の返却日がnilではない場合、trueを返しnilの場合falseを返す
+    !book.latest_lending.returned_date.nil?
+  end
+  
+  #引数bookを渡し、その最新の貸出情報から返却予定日を返す。lendingがnilの場合はnilを返す。
+  def schedule_date_for_lending(book)
+    book.latest_lending&.schedule_date
+  end
+
 end
