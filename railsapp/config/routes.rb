888cca15d9a8a376094fc0216f95812ace3bd410
natsuki-saito <nasushian@gmail.com>
2025-05-17 00:13:25 +0900

    letter_opener_webを導入する #48

--- railsapp/config/routes.rb
+++ railsapp/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
+  
   get "likes/create"
   get "likes/destroy"
   get "reviews/create"
