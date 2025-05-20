6397f894d235959135d48e192099c5d331f35a2e
natsuki-saito <nasushian@gmail.com>
2025-05-20 17:07:39 +0900

    アカウント登録・メールアドレス変更時に認証メールを送信できるようにする。 #49

---
+++ railsapp/app/mailers/users/mailer.rb
@@ -0,0 +1,17 @@
+class Users::Mailer < Devise::Mailer
+  helper :application
+  include Devise::Controllers::UrlHelpers
+  default template_path: 'devise/mailer'
+
+  def confirmation_instructions(record, token, opts={})
+    puts "comfirmation_instructionsが呼び出された"
+    if record.unconfirmed_email != nil
+      opts[:subject] = "メールアドレス変更手続きを完了してください。"
+    else
+      opts[:subject] = "メールアドレス認証を完了してください。"
+    end
+
+    super
+  end
+
+end
--- railsapp/db/schema.rb
+++ railsapp/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_05_01_075431) do
+ActiveRecord::Schema[7.2].define(version: 2025_05_19_064727) do
   create_table "books", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "title", null: false
     t.datetime "created_at", null: false
@@ -62,6 +62,10 @@ ActiveRecord::Schema[7.2].define(version: 2025_05_01_075431) do
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.boolean "admin", default: false
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
