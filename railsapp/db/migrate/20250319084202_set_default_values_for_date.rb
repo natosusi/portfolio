9a3736b96f8971d6cb9e5f6da59f81ee707f2cec
natsuki-saito <nasushian@gmail.com>
2025-05-26 13:09:53 +0900

    不要なファイルの削除 #58

--- railsapp/db/migrate/20250319084202_set_default_values_for_date.rb
+++
@@ -1,8 +0,0 @@
-class SetDefaultValuesForDate < ActiveRecord::Migration[7.2]
-  def up
-    Loan.update_all(borrowed_date: Date.current, schedule_date: Date.current, returned_date: Date.current)
-  end
-
-  def down
-  end
-end
--- railsapp/db/migrate/20250425091344_add_column_to_books.rb
+++
@@ -1,7 +0,0 @@
-class AddColumnToBooks < ActiveRecord::Migration[7.2]
-  def change
-    add_column :books, :author, :string
-    add_column :books, :description, :text
-    add_column :books, :image_link, :string
-  end
-end
