a65b816675b75b6d72fb940d338c3ae15b193042
natsuki-saito <nasushian@gmail.com>
2025-05-26 16:06:18 +0900

    Rspec、FactoryBot、Faker導入 #58

--- railsapp/config/application.rb
+++ railsapp/config/application.rb
@@ -25,5 +25,13 @@ module Railsapp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.generators do |g|
+      g.test_framework :rspec, # テストフレームワークとしてRSpecを指定
+      request_specs: false, # リクエストスペックを作成しない
+      fixtures: false, # テストデータを作るfixtureを作成しない
+      view_specs: false, # ビュー用のスペックを作成しない
+      helper_specs: false, # ヘルパー用のスペックを作成しない
+      routing_specs: false # ルーティングのスペックを作成しない
+      end
   end
 end
