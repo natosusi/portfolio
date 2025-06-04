8831a3e073adb11d10ddf71d9b1e008d79651b77
natsuki-saito <nasushian@gmail.com>
2025-06-04 13:55:22 +0900

    notificationテーブル、モデル作成 #57

--- railsapp/app/models/lending.rb
+++ railsapp/app/models/lending.rb
@@ -1,6 +1,7 @@
 class Lending < ApplicationRecord
   belongs_to :user
   belongs_to :book
+  has_one :notification, dependent: :destroy
   validates :schedule_date, presence: true
 
   #現在貸出中になっている貸出情報
