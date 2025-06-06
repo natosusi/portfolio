10d425d150e6eb194e96d09b2fdc1662f025e3a9
natsuki-saito <nasushian@gmail.com>
2025-06-06 13:23:42 +0900

    lendingモデルの修正、notificationモデルのバリデーション追加 #57

--- railsapp/app/models/notification.rb
+++ railsapp/app/models/notification.rb
@@ -1,7 +1,8 @@
 class Notification < ApplicationRecord
   belongs_to :user
   belongs_to :lending
-  validates :action_type, uniqueness: { scope: :lending }
+  validates :action_type, presence: true, uniqueness: { scope: :lending }
+  validates :checked, inclusion: { in: [true, false] }
 
   scope :unchecked, -> { where(checked: false) }
 end
