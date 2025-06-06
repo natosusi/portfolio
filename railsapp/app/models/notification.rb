a20406cc9907d1027dcf31dd6123bd96f2bb4300
natsuki-saito <nasushian@gmail.com>
2025-06-06 20:30:42 +0900

    notificationモデルのバリデーション追加 #57

--- railsapp/app/models/notification.rb
+++ railsapp/app/models/notification.rb
@@ -1,7 +1,7 @@
 class Notification < ApplicationRecord
   belongs_to :user
   belongs_to :lending
-  validates :action_type, presence: true, uniqueness: { scope: :lending }
+  validates :action_type,{ presence: true, uniqueness: { scope: :lending }, length: { in: 1..3 } }
   validates :checked, inclusion: { in: [true, false] }
 
   scope :unchecked, -> { where(checked: false) }
