04049f954fa1ce6e96d9ed111f6d14c623554ec7
natsuki-saito <nasushian@gmail.com>
2025-06-11 03:59:35 +0900

    fix: checkedカラム削除 #57

--- railsapp/app/models/notification.rb
+++ railsapp/app/models/notification.rb
@@ -2,7 +2,6 @@ class Notification < ApplicationRecord
   belongs_to :user
   belongs_to :lending
   validates :action_type,{ presence: true, uniqueness: { scope: :lending }, length: { in: 1..3 } }
-  validates :checked, inclusion: { in: [true, false] }
 
   scope :not_returned, -> { joins(:lending).merge(Lending.currently_lendings) }
 end
