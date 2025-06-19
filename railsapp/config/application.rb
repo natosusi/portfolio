ffa7e90cba8b6bde2636fe5b196433e80b30bfb8
natsuki-saito <nasushian@gmail.com>
2025-06-20 04:09:13 +0900

    add: Sidekiq、Redisの導入 #75

--- railsapp/config/application.rb
+++ railsapp/config/application.rb
@@ -32,6 +32,8 @@ module Railsapp
         view_specs: false,
         helper_specs: false,
         routing_specs: false
-      end
+    end
+
+    config.active_job.queue_adapter = :sidekiq
   end
 end
