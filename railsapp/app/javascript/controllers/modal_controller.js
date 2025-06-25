b3b3a27523028b22cfad7288e2fac7cd85e7f4e1
natsuki-saito <nasushian@gmail.com>
2025-06-25 18:46:59 +0900

    add: モーダルのStimulusコントローラーを追加 #63

---
+++ railsapp/app/javascript/controllers/modal_controller.js
@@ -0,0 +1,25 @@
+import { Controller } from '@hotwired/stimulus';
+import { Modal } from 'bootstrap';
+
+export default class extends Controller {
+  connect() {
+    let backdrop = document.querySelector('.modal-backdrop');
+    if (backdrop) {
+      backdrop.remove();
+    }
+
+    this.modal = new Modal(this.element);
+    this.modal.show();
+    this.element.addEventListener('hidden.bs.modal', (event) => {
+      this.element.remove();
+      document.body.style.overflow = '';
+      document.body.style.paddingRight = '';
+    });
+  }
+
+  close(event) {
+    if (event.detail.success) {
+      this.modal.hide();
+    }
+  }
+}
